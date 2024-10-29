3cc6a35824163cd1fe5a7e149b694c9fff8e41f2
abdessalamboulayat <abdessalam6boulayat@gmail.com>
2024-10-29 03:13:08 +0100

    feat: add aside profile

---
+++ frontend/src/app/user-profile/aside-profile/aside-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {MatIcon} from "@angular/material/icon";
+
+@Component({
+  selector: 'app-aside-profile',
+  standalone: true,
+    imports: [
+        MatIcon
+    ],
+  templateUrl: './aside-profile.component.html',
+  styleUrl: './aside-profile.component.css'
+})
+export class AsideProfileComponent {
+
+}
