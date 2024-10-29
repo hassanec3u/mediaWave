9776b86f03d6754ccde7b391291170874e171617
abdessalamboulayat <abdessalam6boulayat@gmail.com>
2024-10-29 03:17:47 +0100

    config: environments apis backend

---
+++ frontend/src/environments/environments.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: false,
+    backend: {
+        protocol: 'http',
+        host: 'localhost',
+        port: '3000',
+        endpoints: {
+        },
+    },
+};
